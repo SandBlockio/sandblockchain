ea2f38c03d868cc61fbb4e2c563a31c128c9f8a1
tyldar <tyldar@users.noreply.github.com>
2020-03-17 12:52:14 +0100

    Added back cors middleware

--- x/surprise/client/rest/rest.go
+++ x/surprise/client/rest/rest.go
@@ -8,6 +8,7 @@ import (
 
 // RegisterRoutes registers surprise-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
+	r.Use(mux.CORSMethodMiddleware(r))
 	registerQueryRoutes(cliCtx, r)
 	registerTxRoutes(cliCtx, r)
 }
